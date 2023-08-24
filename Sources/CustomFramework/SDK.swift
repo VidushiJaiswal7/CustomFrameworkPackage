e4b516ba7748647f93e7e852f5c5c3f93fba4f12
Vidushi Jaiswal <vidushi.jaiswal7@gmail.com>
2023-08-24 14:39:58 +0530

    first commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFramework
+//
+//  Created by Vidushi Jaiswal on 24/08/23.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomeWork() {
+        print("Do some work")
+    }
+}
